8146e32cc7fafe9cc7414ba5bcd581ddf4ed0a81
Perepelytsia <perepelytsa@devoler.com>
2019-06-19 03:45:11 +0300

    Fix controller

--- api/controller.py
+++ api/controller.py
@@ -3,44 +3,51 @@ sys.path.append(os.path.join(os.getcwd(), 'actions'))
 
 class Controller:
 
-    def __init__( cmd: dict):
+    def __init__(cmd: dict):
         self.command = cmd
 
-	def createServerCmd(cmd: str, response: dict):
-    	return {"cmd":cmd, "time":"2018-08-31T00:00:00.1", "data":response}
+    def createServerCmd(self, response: dict) -> dict:
+        # save command to the database
+        answer = {"data": response}
+        answer['owner'] = self.command['owner']
 
-	def action(self):
+        if self.command['cmd'] == 'init':
+            answer['cmd'] = 'user'
+        elif self.command['cmd'] == 'finish':
+            answer['cmd'] = 'user'
+        elif self.command['cmd'] == 'start':
+            answer['cmd'] = 'state'
+        elif self.command['cmd'] == 'move':
+            answer['cmd'] = 'state'
+        elif self.command['cmd'] == 'error':
+            answer['cmd'] = 'error'
 
-	    try:
+        return answer
 
-	        if not isinstance(self.command, dict):
-	            raise ValueError("Invalid json for command")
-	        if 'cmd' not in self.command:
-	            raise ValueError('No send cmd')
-	        if 'device' not in self.command:
-	            raise ValueError('No send device')
-	        if 'time' not in self.command:
-	            raise ValueError('No time for command')
-	        if 'data' not in self.command:
-	            raise ValueError('No data for command')
-
-	    except BaseException as err:
-	        import error
-	        response = error.action({"msg": err})
-	        return createServerCmd("error", response)
-	    else:
-	       
-	        if self.command['cmd'] == 'init':
-	            import init
-	            response = init.action(self.command['data']);
-	        elif self.command['cmd'] == 'start':
-	            import start
-	            response = start.action(self.command['data']);
-	        elif self.command['cmd'] == 'move':
-	            import move
-	            response =  move.action(self.command['data']);
-	        elif data['cmd'] == 'finish':
-	            import finish
-	            response =  finish.action(self.command['data']);
-	        
-	        return createServerCmd(self.command['cmd'], response)
+    def action(self) -> dict:
+        try:
+            if not isinstance(self.command, dict):
+                raise ValueError("Invalid json for command")
+            if 'cmd' not in self.command:
+                raise ValueError('No send cmd')
+            if 'owner' not in self.command:
+                raise ValueError('No owner for command')
+            if 'data' not in self.command:
+                raise ValueError('No data for command')
+        except BaseException as err:
+            return createServerCmd("error", {"msg": err})
+        else:
+            # save command to the database
+            if self.command['cmd'] == 'init':
+                import init
+                response = init.action(self.command['data'])
+            elif self.command['cmd'] == 'start':
+                import start
+                response = start.action(self.command['data'])
+            elif self.command['cmd'] == 'move':
+                import move
+                response =  move.action(self.command['data'])
+            elif self.command['cmd'] == 'finish':
+                import finish
+                response =  finish.action(self.command['data'])
+        return createServerCmd(response)
